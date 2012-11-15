036e6b86e90874397ddc2e2adedcaa190760b6fe
Grajalesmx <Grajalesmx@172.20.10.3>
2012-11-14 19:49:24 -0600

    Regresar Cadena Coincidente

--- src/LCS/LCS.java
+++ src/LCS/LCS.java
@@ -8,21 +8,27 @@ public class LCS {
 
     private static int[][] L; // Matriz de Coicidencias
     private static int k; // Variable para almacenar numero de Coincidencias
-    private static int[] XY = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    private static int A = 0;
+    private static int[] XY; // Arreglo para Cadena 
+    private static int A = 0;// Variable auxiliar
 
     public static void main(String[] args) {
         int X[] = {1, 7, 4, 6, 2, 5, 8};    //Cadena a Comparar
         int Y[] = {1, 4, 8, 6, 4, 5, 2, 8}; //Cadena a Comparar
+//        String X[]={"o","s","i","t","o","b","i","m","b","o"};
+//        String Y[]={"o","s","o","b","i","b","o"};
 
         L = new int[X.length + 1][Y.length + 1]; //Inicializar el arreglo de Tamaño n,m
 
         k = LCS.LCS(X, Y);
+        
+        System.out.println("");
         System.out.println(k); //Imprime el numero de Coincidencias
+        
+        System.out.println(""+LCS.Backtrack(L,X,Y,X.length,Y.length));        
     }
 
     public static int LCS(int[] X, int[] Y) {
-
+//    public static int LCS(String[] X, String[] Y) {
         int m = X.length; //Numero de elementos en la cadena X
         int n = Y.length; //Numero de elementos en la cadena Y
 
@@ -39,9 +45,10 @@ public class LCS {
             for (int j = 1; j <= Y.length; j++) {
 
                 //Coincidencias
-                if (X[i - 1] == Y[j - 1]) {
+                if (X[i - 1]==(Y[j - 1])) {
+//                    if (X[i - 1] == Y[j - 1]) {
                     L[i][j] = L[i - 1][j - 1] + 1; // Suma 1 al valor de la Diagonal izq.
-                    System.out.print(X[i - 1]);
+
                 } // No Coincidencias
                 else {
                     L[i][j] = Math.max(L[i - 1][j], L[i][j - 1]); //Obtiene el numero mayor entre el numero superior y el izq.
@@ -52,7 +59,26 @@ public class LCS {
         return L[m][n]; // Regresa el valor ubicado al final de la matriz con el numero mayor de coincidencias
     }
 
-    public void backtrack() {
-        
+    public static int Backtrack(int[][] L, int[] X, int[] Y, int i, int j) {
+
+    if( i == 0 || j == 0){
+     }
+    else{ 
+        if (X[i]==(Y[j])){
+        return Backtrack(L, X, Y, i-1, j-1) + X[i];
+   }
+        else{
+     if (L[i,j-1] > L[i-1,j])
+             //(L[i,j-1] > L[i-1,j])
+     {
+           return Backtrack(L, X, Y, i, j-1);
+       }else{
+           return Backtrack(L, X, Y, i-1, j);   
     }
+    }
+    }
+return 0;
 }
+
+
+    }
